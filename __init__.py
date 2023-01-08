c5f21e9414cbd4d55e11710c9342464416cb5d0c
manuel richard <107644690+lilnight666@users.noreply.github.com>
2023-01-08 09:40:42 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import _mysql_connector
+import mysql.connector
+
+db_connection = mysql.connector.Connect(host="localhost", user="root", passwd="", database="cadastrotrabaia")
+cursor = db_connection.cursor()
+dados = dict()
+resp=''
+while True:
+        dados['nome'] = str(input("digite o nome do funcionario "))
+        nasc = int(input("ano de nascimento"))
+        dados['idade'] = datetime.now().year - nasc
+        dados['ctps'] = int(input("caterira de trabalho(digite 0 se não tiver:"))
+        resp = str(input("quer continuar cadastrando?(s/n)"))
+        if dados['ctps'] != 0:
+            dados['contratação'] = int(input('ano de contaratação'))
+            dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+resp= str(input("quer continuar cadastrando?(s/n)"))
+if resp == 'nN':
+
+
+
+
+
+sql = (f"""INSERT INTO pessoas(nome,idade,ctps,aposentadoria,contratação) 
+VALUES ('{dados['nome']}','{dados['idade']}','{dados['ctps']}','{dados['aposentadoria']}','{dados['contratação']}')""")
+cursor.execute(sql)
+cursor.close()
+db_connection.commit()
+db_connection.close()
